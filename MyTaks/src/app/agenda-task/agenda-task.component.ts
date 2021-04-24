c717c35adba531a1ed0757fb40f9912cb1e13d40
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2021-04-23 22:05:18 -0300

    Adicionar a data da tarefa quando ela for realizada nos próximos dias. As tarefas das seções “Meu Dia” ou “Amanhã” não devem apresentar a data.

--- MyTaks/src/app/agenda-task/agenda-task.component.ts
+++ MyTaks/src/app/agenda-task/agenda-task.component.ts
@@ -6,15 +6,14 @@ import { TaskService } from '../services/task.service';
 @Component({
   selector: 'app-agenda-task',
   templateUrl: './agenda-task.component.html',
-  styleUrls: ['./agenda-task.component.scss']
+  styleUrls: ['./agenda-task.component.scss'],
 })
 export class AgendaTaskComponent implements OnInit {
   @Input() tasks: Task[] = [];
 
-  constructor(private taskServe: TaskService) { }
+  constructor(private taskServe: TaskService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   done(task: Task, event: MatCheckboxChange) {
     this.taskServe.alterTask(task.id, task).subscribe();
@@ -22,5 +21,18 @@ export class AgendaTaskComponent implements OnInit {
 
   deleteTask(taskId: number | undefined) {
     this.taskServe.deleteTask(taskId).subscribe();
+  }
+  dataExibir(dataTarefa: Date, dias: number) {
+    //recuperando data atual, e somando os dias conforme parametro esperado
+    var dataSomanda = new Date();
+    dataSomanda.setDate(dataSomanda.getDate() + dias - 1);
+
+    //formatando a dataTarefa do formatro isostring para date para comparação.
+    var dataTarefaFormatada = new Date(dataTarefa);
+
+    if (dataTarefaFormatada >= dataSomanda) {
+      return true;
+    }
+    return false;
   }
 }
